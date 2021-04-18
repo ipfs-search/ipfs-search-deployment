e0f343e3698b73e73cd01616f260b198852f1947
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-04-18 16:41:18 +0100

    Scripted WIP garbage collection for IPFS.

---
+++ roles/ipfs-gc/templates/ipfs_gc.sh
@@ -0,0 +1,18 @@
+#$!/bin/bash
+set -x -e
+
+BADGER="{{ badger_binary }}"
+export IPFS_PATH="{{ ipfs_path }}"
+systemctl stop ipfs {{ ipfs_dependant_services }}
+sudo -u {{ ipfs_user }} bash -c "ulimit -Sn 65536; $BADGER flatten --dir {{ ipfs_path }}/badgerds"
+systemctl start ipfs {{ ipfs_dependant_services }}
+sleep 30
+ipfs repo gc
+ipfs repo stat -H
+sleep 30
+ipfs repo gc
+ipfs repo stat -H
+sleep 5
+sync
+sleep 5
+fstrim -a
