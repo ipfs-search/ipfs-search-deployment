0679bbb5963a3fbed0f5deb02fba5aacab0222fa
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-06-20 13:38:29 +0100

    Fixups in fixups garbage collection.

--- roles/ipfs-gc/templates/ipfs_gc.sh
+++ roles/ipfs-gc/templates/ipfs_gc.sh
@@ -2,34 +2,44 @@
 set -e
 
 BADGER="{{ badger_binary }}"
+IPFS_USER="{{ ipfs_user }}"
+IPFS="sudo -u $IPFS_USER -E ipfs"
 export IPFS_PATH="{{ ipfs_path }}"
 
-echo "Cleaning repo:"
-ipfs repo gc -q | wc -l
-echo "items deleted"
-ipfs repo stat -H
-sleep 30
-echo "Stat 30s later"
-ipfs repo stat -H
+echo "Repo stat:"
+$IPFS repo stat -H
+
+echo "Garbage-collecting repo:"
+DELETED=`$IPFS repo gc -q | wc -l`
+echo "$DELETED items deleted"
+
+echo "Repo stat:"
+$IPFS repo stat -H
 
 echo "Stopping IPFS"
 systemctl stop ipfs
 
 echo "Flattening repo"
-sudo -u {{ ipfs_user }} bash -c "ulimit -Sn 65536; $BADGER flatten --dir {{ ipfs_path }}/badgerds > /dev/null"
+sudo -u $IPFS_USER bash -c "ulimit -Sn 65536; $BADGER flatten --dir {{ ipfs_path }}/badgerds > /dev/null"
 
-echo "Restarting IPFS"
+echo "Starting IPFS"
 systemctl start ipfs
 
 echo "Starting dependant services"
 systemctl list-dependencies --reverse --plain ipfs | sed -n "s/^  \(\S*\)\.service$/\1/p" | xargs systemctl start
 
-ipfs repo stat -H
+echo "Repo stat:"
+$IPFS repo stat -H
+
+echo "Garbage-collecting repo:"
+DELETED=`$IPFS repo gc -q | wc -l`
+echo "$DELETED items deleted"
+
 sleep 30
-echo "Cleaning repo:"
-ipfs repo gc -q | wc -l
-echo "items deleted"
-ipfs repo stat -H
+
+echo "Repo stat:"
+$IPFS repo stat -H
+
 sleep 5
 echo "Synching disk buffers"
 sync
