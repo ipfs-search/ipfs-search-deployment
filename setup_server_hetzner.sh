f99d30257bc59c10c720e7635e5a2a68e6e6481d
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-11-11 15:23:13 +0000

    Remove legacy Hetzner setup script.

--- setup_server_hetzner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-HOST=$1
-
-echo 'Installing image'
-env ANSIBLE_HOST_KEY_CHECKING=False ansible $HOST -u root -a "/root/.oldroot/nfs/install/installimage -r yes -l 0 -i /root/.oldroot/nfs/install/../images/Ubuntu-2004-focal-64-minimal.tar.gz -g -t yes -G yes -a && shutdown -r now"
-
-echo 'Bootstrapping Ansible'
